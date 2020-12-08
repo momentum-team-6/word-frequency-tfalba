b3735370f05f5d9c1097cf79efa9cd143df6bf16
Tracy Falba <tfalba@mac.com>
2020-12-08 13:45:57 -0500

    created count dictionary and removed punctuation and capitalization

--- word_frequency.py
+++ word_frequency.py
@@ -3,6 +3,35 @@ STOP_WORDS = [
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
+PUNC = '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
+
+fileObject = open('praise_song_for_the_day.txt', 'r')
+data = fileObject.read()
+# print(data)
+# print(type(fileObject))
+# print(type(data))
+
+def word_count(str):
+    counts = dict()
+    words = str.split()
+
+    for word in words:
+        l_word = word.lower()
+        for elem in l_word:
+            if elem in PUNC:
+                l_word = l_word.replace(elem, '')
+        if l_word not in STOP_WORDS:
+
+            if l_word in counts:
+                counts[l_word] += 1
+            else:
+                counts[l_word] = 1
+    print(type(counts))
+    print(counts.values())
+    return counts
+    
+
+print(word_count(data))
 
 
 def print_word_freq(file):
@@ -10,18 +39,18 @@ def print_word_freq(file):
     pass
 
 
-if __name__ == "__main__":
-    import argparse
-    from pathlib import Path
+# if __name__ == "__main__":
+#     import argparse
+#     from pathlib import Path
 
-    parser = argparse.ArgumentParser(
-        description='Get the word frequency in a text file.')
-    parser.add_argument('file', help='file to read')
-    args = parser.parse_args()
+#     parser = argparse.ArgumentParser(
+#         description='Get the word frequency in a text file.')
+#     parser.add_argument('file', help='file to read')
+#     args = parser.parse_args()
 
-    file = Path(args.file)
-    if file.is_file():
-        print_word_freq(file)
-    else:
-        print(f"{file} does not exist!")
-        exit(1)
+#     file = Path(args.file)
+#     if file.is_file():
+#         print_word_freq(file)
+#     else:
+#         print(f"{file} does not exist!")
+#         exit(1)
