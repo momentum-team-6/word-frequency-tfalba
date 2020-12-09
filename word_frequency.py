c805b0377ae1df4a023785409834a945841dbdfd
Tracy Falba <tfalba@mac.com>
2020-12-09 07:03:28 -0500

    added comments to explain code

--- word_frequency.py
+++ word_frequency.py
@@ -7,12 +7,16 @@ PUNC = '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    
+#create a string from read file and replace caps with lower
     read_file = open(file, 'r').read().lower()
+#words separated by hyphens are replaced with white space
     read_file = read_file.replace('—', ' ')
-    read_file = read_file.replace('-', ' ') 
+#create a list words split at white space of read_file
     words = read_file.split()
 
+#replace punctuation elements
+#create dictionary counts and add each word in words to it and increment count
+#skip over words that are in STOP_WORDS
     counts = dict()
     for word in words:
         for elem in word:
@@ -23,15 +27,19 @@ def print_word_freq(file):
                 counts[word] += 1
             else:
                 counts[word] = 1
-    import operator
     #sorted_keys = sorted(counts.items())
     #counts_dict = dict(sorted_keys)
+    import operator
     sorted_counts = sorted(counts.items(), key=operator.itemgetter(1))
+    # Set star_display, word_length, and padded_key lists
     star_display = []
-    longest_word_length = len(sorted_counts[0][0])
     word_length = []
     padded_key = []
+    # Initial longest_word)length
+    longest_word_length = len(sorted_counts[0][0])
 
+
+# Initialize star and padded_key arrays and find longest_word_length
     for i in range(len(sorted_counts)):
         len(sorted_counts[i][0])
         star_display.append('')
@@ -40,12 +48,15 @@ def print_word_freq(file):
         if i > 0:
             if len(sorted_counts[i][0])>longest_word_length:
                 longest_word_length = word_length[i]
+
+# Inner loops to assign padding to padded_key and set number of *'s for display
     for i in range(len(sorted_counts)):
         for j in range(longest_word_length - word_length[i]): 
             padded_key[i] = ' ' + padded_key[i]
         for k in range(int(sorted_counts[i][1])):
             star_display[i] += '*'
-        
+
+# Loop to print in descending order of word count        
     for i in reversed(range(len(sorted_counts))):
         if int(sorted_counts[i][1]) > 1:
             print(padded_key[i], '|', sorted_counts[i][1], star_display[i])
